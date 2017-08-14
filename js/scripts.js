339e56cfccbbe92a95af0944694a59d4dec56cac
lainey rokkan and rebecca mcintosh <rebecca@>
2017-08-14 13:14:12 -0700

    add a splice to the result list and give us a new array

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,9 @@ $(document).ready(function() {
     var band = $('input#band').val();
     var movie = $('input#movie').val();
     var list = [color, food, animal, curse, band, movie];
-
-    $("#result").text(list);
+    var zero = list.splice(0, 3);
+    var newArray = zero;
+    $("#result").text(newArray);
     $("#array").show();
 
     event.preventDefault();
