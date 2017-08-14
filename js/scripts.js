9f39973e0300484a3b8b1ecdcaadc742da7888be
lainey rokkan and rebecca mcintosh <lainey@>
2017-08-14 10:57:57 -0700

    complete the assignment of displaying the array result from an html form

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var color = $('input#color').val();
+    var food = $('input#food').val();
+    var animal = $('input#animal').val();
+    var curse = $('input#curse').val();
+    var band = $('input#band').val();
+    var movie = $('input#movie').val();
+    var list = [color, food, animal, curse, band, movie];
+    $("#result").text(list);
+    $("#array").show();
+
+    event.preventDefault();
+  });
+});
